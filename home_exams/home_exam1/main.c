a567611086d4d3e20a4aa4f0d037d802f84e4425
reneaas <reneaas@student.matnat.uio.no>
2020-02-27 23:33:55 +0100

    now the reading is fast and concise

--- home_exams/home_exam1/main.c
+++ home_exams/home_exam1/main.c
@@ -12,16 +12,37 @@ int main(int argc, char const *argv[]) {
   int max_line_length = 128;
   //char *filename = "8-webpages.txt";
   char *filename = "web-NotreDame.txt";
-  char *line_buffer[max_line_length];
-  char *line_buffer2[max_line_length];
+  char *line_buffer = (char*)malloc(max_line_length*sizeof(char));
   FILE *fp = fopen(filename, "r");
   char *token1, *token2;
   const char *N_string;
   int parameters[2];
-  const char *delimiter1 = "Nodes:";
-  const char *delimiter2 = "Edges:";
+  const char *delimiter1 = "Nodes: ";
+  const char *delimiter2 = "Edges: ";
 
 
+  for (int i = 0; i < 3; i++){
+    fgets(line_buffer, max_line_length, fp);
+  }
+
+  char buffer[1024];
+  token1 = strtok(line_buffer, delimiter1);
+  int iter, counter;
+  iter = 0; counter = 0;
+  while (token1 != NULL){
+    strcpy(buffer, token1);
+    if (isdigit(buffer[0]) != 0 && atoi(&buffer[0]) != 0)
+    {
+      parameters[counter] = atoi(&buffer[0]);
+      counter++;
+    }
+    if (counter == 2) break;
+    token1 = strtok(NULL, delimiter1);
+  }
+
+
+
+  /*
   //The line of interest is at line 3 so we read until we get the third line.
   for (int i = 0; i < 3; i++){
     fgets(line_buffer, max_line_length, fp);
@@ -57,7 +78,7 @@ int main(int argc, char const *argv[]) {
     if (counter == 2) break;       //Once two numbers are found, we break the loop.
     l++;
   }
-
+  */
   int N = parameters[0];                //Number of nodes.
   int N_links = parameters[1];          //Number of links
   printf("Number of nodes = %d\n", N);
